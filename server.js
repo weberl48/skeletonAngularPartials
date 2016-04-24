676787ace229be04b3097c09d259a4187dbed323
weberl48 <weberl48@outlook.com>
2016-04-24 18:01:25 -0400

    set up, building angular routes

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var stylus = require('stylus');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var app = express();
+
+function compile(str, path) {
+  return stylus(str).set('filename', path);
+}
+
+app.set('views', __dirname + '/server/views');
+app.set('view engine', 'jade');
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(stylus.middleware(
+  {
+    src: __dirname + '/public',
+    compile: compile
+  }
+));
+app.use(express.static(__dirname + '/public'));
+
+app.get('*' , function(req, res) {
+  res.render('index')
+})
+
+
+var port = process.env.PORT || 3030;
+app.listen(port);
+console.log('Listening on port ' + port);
